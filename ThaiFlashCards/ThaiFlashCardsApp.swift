169f9f2bd19036ac3e2583388c83326d53a8143b
Dylan Elliott <dylan.elliott@gmail.com>
2023-08-26 18:18:25 +1000

    Initial Commit

---
+++ ThaiFlashCards/ThaiFlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThaiFlashCardsApp.swift
+//  ThaiFlashCards
+//
+//  Created by Dylan Elliott on 26/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct ThaiFlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
