59625a83f490265c70b505cb20b8a0fb9ffe106f
Dylan Elliott <dylan.elliott@gmail.com>
2023-09-12 01:07:52 +1000

    Add app icon

---
+++ ThaiFlashCards/Card View/CardFaceContent.swift
@@ -0,0 +1,25 @@
+//
+//  CardFace.swift
+//  ThaiFlashCards
+//
+//  Created by Dylan Elliott on 11/9/2023.
+//
+
+import Foundation
+
+enum CardFaceContent {
+    case regularText(String)
+    case sectionedText([(title: String, value: String)])
+    case largeText(String)
+    case largeTextWithFooter(String, String)
+    
+    var text: String {
+        switch self {
+        case let .regularText(text): return text
+        case let .sectionedText(text): return text.map { "\($0.title): \($0.value)" }.joined(separator: ". ")
+        case let .largeText(text): return text
+        case let .largeTextWithFooter(text, footer): return "\(text), \(footer)"
+        }
+    }
+}
+
